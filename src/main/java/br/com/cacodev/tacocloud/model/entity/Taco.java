ea156bbf1759b5d22ae12e34487dcac4bb1df23d
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2019-08-06 21:51:34 -0300

    Controladores e validação dos campos

--- src/main/java/br/com/cacodev/tacocloud/model/entity/Taco.java
+++ src/main/java/br/com/cacodev/tacocloud/model/entity/Taco.java
@@ -4,12 +4,19 @@ package br.com.cacodev.tacocloud.model.entity;
 
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class Taco{
 
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characteres long")
     private String name;
+
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<String> ingredients;
 
 }
--- src/main/java/br/com/cacodev/tacocloud/web/controller/OrderController.java
+++ src/main/java/br/com/cacodev/tacocloud/web/controller/OrderController.java
@@ -1,8 +1,11 @@
 
 package br.com.cacodev.tacocloud.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +25,10 @@ public class OrderController{
     }
 
     @PostMapping
-    public String processsOrder(Order order){
+    public String processsOrder( @Valid Order order, Errors errors){
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
